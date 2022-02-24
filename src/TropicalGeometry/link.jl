64b02ba248f635414bfab0d0e0f2aabe6b57d5e6
Yue Ren <yue.ren.kl@gmail.com>
2022-02-24 16:49:52 +0000

    TropicalGeometry: tropical_link, fixed bug arising over p-adics (not sure why)

--- src/TropicalGeometry/link.jl
+++ src/TropicalGeometry/link.jl
@@ -153,7 +153,8 @@ end
 #=======
 tropical_link
 Example:
-Kx,(x1,x2,x3,x4,x5) = PolynomialRing(QQ,5)
+K = GF(32003)
+Kx,(x1,x2,x3,x4,x5) = PolynomialRing(K),5)
 # inI = ideal([-10*x2^2*x3+9*x1*x3^2+31*x2*x3^2-25*x1*x3*x4-14*x2^2*x5+6*x1*x5^2+46*x2*x5^2-32*x4*x5^2,
 #              10*x1^3-2*x1^2*x2+38*x1^2*x3+6*x2^2*x3+34*x1*x2*x4-x3^2*x4+8*x1*x4^2+18*x2*x4^2+47*x3^2*x5-22*x4^2*x5+43*x2*x5^2+16*x3*x5^2,
 #              49*x1^2*x4-50*x1*x2*x4-20*x1*x4^2+40*x1*x2*x5-27*x1*x3*x5+4*x1*x4*x5+22*x3*x5^2])
@@ -202,7 +203,7 @@ function tropical_link(inI::MPolyIdeal; local_precision::Integer=19)
   append!(hyperplanes,[t*x[i]-1 for i in nonpivotIndices])
   rayGenerators = [];
   for hyperplane in hyperplanes
-    inI0 = inI1+ideal(Lx,hyperplane)
+    inI0 = inI1+ideal(Lx,[hyperplane])
 
     ###
     # Optional: Compute a partially saturated GB using satstd
